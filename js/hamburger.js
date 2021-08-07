a16535d45afb923c3aa3e40671d85df894ee26db
今村一貴 <imamurakazuki@imamurakazutakanoMacBook-Pro.local>
2021-08-07 14:34:19 +0900

    css

--- js/hamburger.js
+++ js/hamburger.js
@@ -14,24 +14,40 @@ $(function () {
 
 
 });
+$(function () {
+
+    $(".modal").on( 'click', function () {
+
+
+       
+        $("html").toggleClass("is-open")
+       
+
+    });
+    
+    
+    
+
+
+});
 
 
 
 
    // 画面リサイズでリロードさせる設定
-   var timer = 0;
-   var currentWidth = window.innerWidth;
- $(window).resize(function(){
-       if (currentWidth == window.innerWidth) {
-           return;
-       }
-       if (timer > 0) {
-           clearTimeout(timer);
-       }
+//    var timer = 0;
+//    var currentWidth = window.innerWidth;
+//  $(window).resize(function(){
+//        if (currentWidth == window.innerWidth) {
+//            return;
+//        }
+//        if (timer > 0) {
+//            clearTimeout(timer);
+//        }
 
-       timer = setTimeout(function () {
-           location.reload();
-       }, 200);
+//        timer = setTimeout(function () {
+//            location.reload();
+//        }, 200);
 
- });
+//  });
 
