fc59b802338230260d631385df1edc52554459da
今村一貴 <imamurakazuki@imamurakazutakanoMacBook-Pro.local>
2021-08-08 17:48:31 +0900

    css

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,53 +1,53 @@
 $(function () {
 
-    $(".is-hamburger").on( 'click', function () {
+    $(".is-hamburger").on('click', function () {
 
 
         $(this).toggleClass("is-open")
-        $("body").toggleClass("is-open")
-       
-
-    });
-    
-    
-    
-
-
-});
-$(function () {
-
-    $(".modal").on( 'click', function () {
-
-
-       
         $("html").toggleClass("is-open")
-       
+
 
     });
-    
-    
-    
+
+
+
 
 
 });
+// $(function () {
+
+//     $(".modal").on('click', function () {
+
+
+
+//         $("html").toggleClass("is-open");
+
+
+//     });
 
 
 
 
-   // 画面リサイズでリロードさせる設定
-//    var timer = 0;
-//    var currentWidth = window.innerWidth;
-//  $(window).resize(function(){
-//        if (currentWidth == window.innerWidth) {
-//            return;
-//        }
-//        if (timer > 0) {
-//            clearTimeout(timer);
-//        }
 
-//        timer = setTimeout(function () {
-//            location.reload();
-//        }, 200);
+// });
 
-//  });
+
+
+
+// 画面リサイズでリロードさせる設定
+var timer = 0;
+var currentWidth = window.innerWidth;
+$(window).resize(function () {
+    if (currentWidth == window.innerWidth) {
+        return;
+    }
+    if (timer > 0) {
+        clearTimeout(timer);
+    }
+
+    timer = setTimeout(function () {
+        location.reload();
+    }, 200);
+
+});
 
